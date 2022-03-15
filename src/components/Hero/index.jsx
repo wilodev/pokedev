bd4146e1611506bf71e8bb71e61a1a89a46970ef
wilodev <wilsonperez.developer@gmail.com>
2022-03-15 15:16:15 -0500

    Se añade la carpeta public

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
         <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 ">
           <img
             className="h-56 w-full object-cover sm:h-72 md:h-72 lg:w-full lg:h-full"
-            src="../../assets/pikachu.png"
+            src="./images/pikachu.png"
             alt=""
           />
         </div>
